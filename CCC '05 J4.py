092fdcae1b594eafdc06a38ebd71dd494b37d0da
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-12-16 09:48:10 -0500

    Update CCC '05 J4.py

--- CCC '05 J4.py
+++ CCC '05 J4.py
@@ -18,140 +18,78 @@ def get_grid():
 for x in range (5):
   input_storage.append(int(input()))
 
-currentx, currenty, = input_storage[2], 0
+currentx, currenty, = input_storage[2] + 1, 1
 
+temp_temp_grid = ["#"] * (input_storage[0] + 2)
+grid.append(temp_temp_grid)
 # Initialize grid
 for i in range (input_storage[1]):
-  temp_grid = ["#"] * (input_storage[0] + 2)
-  for j in range (input_storage[0]):
+  temp_grid = [""] * (input_storage[0] + 2)
+  for j in range (input_storage[0] + 2):
     temp_grid[j] = "."
   grid.append(temp_grid)
+grid.append(temp_temp_grid)
 
 # Set up top blocked area
 for x in range (1, input_storage[3] + 1):
   for y in range (1, input_storage[2] + 1):
-    grid[x][0] = "#"
     grid[x][y] = "#"
     grid[x][len(grid[0]) - y - 1] = "#"
 
 # Set up bottom blocked area
-for x in range (len(grid) - input_storage[3], len(grid)):
+for x in range (len(grid) - input_storage[3] - 1, len(grid) - 1):
   for y in range (1, input_storage[2] + 1):
     grid[x][y] = "#"
     grid[x][len(grid[0]) - y - 1] = "#"
 
+for x in range (len(grid)):
+  grid[x][0], grid[x][len(grid[x]) - 1] = "#", "#"
+
 total_moved = 0
+operation = 0
 
-print(len(grid[0]))
-print(len(grid))
-print(input_storage[4])
+grid[currenty][currentx] = "M"
 
-# REMEMBER: it's grid[y][x]
-
-#and currentx < len(grid[0])
-
-while (total_moved != input_storage[4]):
-  grid[currenty][currentx] = "M"
-
-  # Move right
-  if (currentx != len(grid[0]) - 1):
-    print("hi1")
-    while (grid[currenty][currentx + 1] != "#"):
+for x in range (input_storage[4]):
+  if (operation == 0):
+    if (grid[currenty][currentx + 1] != "#"):
       grid[currenty][currentx] = "#"
       currentx += 1
-      if (currentx == len(grid[0]) - 1):
-        break
-      total_moved += 1
-      if (total_moved == input_storage[4]):
-        grid[currenty][currentx] = "M"
-        get_grid()
-        print(currenty + 1, currentx + 1)
-        sys.exit()
-      grid[currenty][currentx] = "M"
-
-  # Go down
-  print("hi2")
-  while (grid[currenty][currentx + 1] == "#"):
-    grid[currenty][currentx] = "#"
-    currenty += 1
-    total_moved += 1
-    if (total_moved == input_storage[4] - 1):
-      grid[currenty - 1][currentx] = "M"
-      get_grid()
-      print(currenty + 1, currentx + 1)
-      sys.exit()
-    grid[currenty][currentx] = "M"
-
-  # Move right
-  if (currentx != len(grid[0]) - 1):
-    print("hi3")
-    while (grid[currenty][currentx + 1] != "#"):
-      grid[currenty][currentx] = "#"
-      currentx += 1
-      if (currentx == len(grid[0]) - 1):
-        break
-      total_moved += 1
-      if (total_moved == input_storage[4] - 1):
-        grid[currenty][currentx] = "M"
-        get_grid()
-        print(currenty + 1, currentx + 1)
-        sys.exit()
-      grid[currenty][currentx] = "M"
-  
-  # Move down
-  print("hi4")
-  while (grid[currenty + 1][currentx] == "."):
-    grid[currenty][currentx] = "#"
-    currenty += 1
-    total_moved += 1
-    if (total_moved == input_storage[4] - 1):
-      grid[currenty][currentx] = "M"
-      get_grid()
-      print(currenty + 1, currentx + 1)
-      sys.exit()
-    grid[currenty][currentx] = "M"
-
-  # Move left
-  if (currenty != 0):
-    print("hi5")
-    while (grid[currenty + 1][currentx] == "#"):
-      grid[currenty][currentx] = "#"
-      currentx -= 1
-      if (currentx == 0):
-        break
-      total_moved += 1
-      if (total_moved == input_storage[4] - 1):
-        grid[currenty][currentx] = "M"
-        get_grid()
-        print(currenty + 1, currentx + 1)
-        sys.exit()
-  
-  # Move down
-  if (currenty == len(grid)):
-    while (grid[currenty][currentx + 1] == "#"):
-      print("hii")
+    elif (grid[currenty][currentx + 1] == "#" and grid[currenty + 1][currentx] != "#"):
       grid[currenty][currentx] = "#"
       currenty += 1
-      if (currenty == len(grid) - 1):
-        break
-      total_moved += 1
-      if (total_moved == input_storage[4] - 1):
-        grid[currenty - 1][currentx] = "M"
-        get_grid()
-        print(currenty + 1, currentx + 1)
-        sys.exit()
+    else:
+      operation = 1
+  if (operation == 1):
+    if (grid[currenty][currentx - 1] != "#" and grid[currenty + 1][currentx] == "#"):
+      grid[currenty][currentx] = "#"
+      currentx -= 1
+    elif (grid[currenty][currentx + 1] == "#" and grid[currenty + 1][currentx] != "#"):
+      grid[currenty][currentx] = "#"
+      currenty += 1
+    else:
+      operation = 2
+  if (operation == 2):
+    if (grid[currenty][currentx - 1] == "#" and grid[currenty - 1][currentx] != "#"):
+      grid[currenty][currentx] = "#"
+      currenty -= 1
+    elif (grid[currenty][currentx - 1] != "#" and grid[currenty + 1][currentx] == "#"):
+      grid[currenty][currentx] = "#"
+      currentx -= 1
+    else:
+      operation = 3
+  if (operation == 3):
+    if (grid[currenty][currentx + 1] != "#" and grid[currenty - 1][currentx] == "#"):
+      grid[currenty][currentx] = "#"
+      currentx += 1;
+    elif (grid[currenty - 1][currentx] != "#" and grid[currenty][currentx - 1] == "#"):
+      grid[currenty][currentx] = "#"
+      currenty -= 1;
+    else:
+      operation = 0;
 
-  while (grid[currenty][currentx + 1] == "#"):
-    grid[currenty][currentx] = "#"
-    currenty += 1
-    total_moved += 1
-    if (total_moved == input_storage[4] - 1):
-      grid[currenty - 1][currentx] = "M"
-      get_grid()
-      print(currenty + 1, currentx + 1)
-      sys.exit()
-    grid[currenty][currentx] = "M"
 
-
-
+grid[currenty][currentx] = "M"
+print(currentx)
+print(currenty)
 get_grid()
