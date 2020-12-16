7aff1835a54c5099db856c8654d45a55db7d8d6b
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-12-16 09:45:57 -0500

    Update CCC '05 J4.py

--- CCC '05 J4.py
+++ CCC '05 J4.py
@@ -1,95 +1,157 @@
-
-# See the symbol table at the bottom of this page.
 import sys
-Niswanob1=[]
-fuscat1=[]
-def Chri2():
- for Chri1 in range(len(fuscat1)):
-  print (fuscat1[Chri1])
+
+input_storage = []
+grid = []
+
+# . = clear
+# # = blocked
+#  make current square #
+#   while true:
+#   while (grid[currentx + 1][currenty ] != "#""): currentx += 1
+#   while (grid[currentx][currenty + 1] != "#"): currenty += 1
+#   while (grid[currentx])
+
+def get_grid():
+  for x in range (len(grid)):
+    print(grid[x])
+
+for x in range (5):
+  input_storage.append(int(input()))
+
+currentx, currenty, = input_storage[2], 0
+
+# Initialize grid
+for i in range (input_storage[1]):
+  temp_grid = ["#"] * (input_storage[0] + 2)
+  for j in range (input_storage[0]):
+    temp_grid[j] = "."
+  grid.append(temp_grid)
+
+# Set up top blocked area
+for x in range (1, input_storage[3] + 1):
+  for y in range (1, input_storage[2] + 1):
+    grid[x][0] = "#"
+    grid[x][y] = "#"
+    grid[x][len(grid[0]) - y - 1] = "#"
+
+# Set up bottom blocked area
+for x in range (len(grid) - input_storage[3], len(grid)):
+  for y in range (1, input_storage[2] + 1):
+    grid[x][y] = "#"
+    grid[x][len(grid[0]) - y - 1] = "#"
+
+total_moved = 0
+
+print(len(grid[0]))
+print(len(grid))
+print(input_storage[4])
+
+# REMEMBER: it's grid[y][x]
+
+#and currentx < len(grid[0])
+
+while (total_moved != input_storage[4]):
+  grid[currenty][currentx] = "M"
+
+  # Move right
+  if (currentx != len(grid[0]) - 1):
+    print("hi1")
+    while (grid[currenty][currentx + 1] != "#"):
+      grid[currenty][currentx] = "#"
+      currentx += 1
+      if (currentx == len(grid[0]) - 1):
+        break
+      total_moved += 1
+      if (total_moved == input_storage[4]):
+        grid[currenty][currentx] = "M"
+        get_grid()
+        print(currenty + 1, currentx + 1)
+        sys.exit()
+      grid[currenty][currentx] = "M"
+
+  # Go down
+  print("hi2")
+  while (grid[currenty][currentx + 1] == "#"):
+    grid[currenty][currentx] = "#"
+    currenty += 1
+    total_moved += 1
+    if (total_moved == input_storage[4] - 1):
+      grid[currenty - 1][currentx] = "M"
+      get_grid()
+      print(currenty + 1, currentx + 1)
+      sys.exit()
+    grid[currenty][currentx] = "M"
+
+  # Move right
+  if (currentx != len(grid[0]) - 1):
+    print("hi3")
+    while (grid[currenty][currentx + 1] != "#"):
+      grid[currenty][currentx] = "#"
+      currentx += 1
+      if (currentx == len(grid[0]) - 1):
+        break
+      total_moved += 1
+      if (total_moved == input_storage[4] - 1):
+        grid[currenty][currentx] = "M"
+        get_grid()
+        print(currenty + 1, currentx + 1)
+        sys.exit()
+      grid[currenty][currentx] = "M"
+  
+  # Move down
+  print("hi4")
+  while (grid[currenty + 1][currentx] == "."):
+    grid[currenty][currentx] = "#"
+    currenty += 1
+    total_moved += 1
+    if (total_moved == input_storage[4] - 1):
+      grid[currenty][currentx] = "M"
+      get_grid()
+      print(currenty + 1, currentx + 1)
+      sys.exit()
+    grid[currenty][currentx] = "M"
+
+  # Move left
+  if (currenty != 0):
+    print("hi5")
+    while (grid[currenty + 1][currentx] == "#"):
+      grid[currenty][currentx] = "#"
+      currentx -= 1
+      if (currentx == 0):
+        break
+      total_moved += 1
+      if (total_moved == input_storage[4] - 1):
+        grid[currenty][currentx] = "M"
+        get_grid()
+        print(currenty + 1, currentx + 1)
+        sys.exit()
+  
+  # Move down
+  if (currenty == len(grid)):
+    while (grid[currenty][currentx + 1] == "#"):
+      print("hii")
+      grid[currenty][currentx] = "#"
+      currenty += 1
+      if (currenty == len(grid) - 1):
+        break
+      total_moved += 1
+      if (total_moved == input_storage[4] - 1):
+        grid[currenty - 1][currentx] = "M"
+        get_grid()
+        print(currenty + 1, currentx + 1)
+        sys.exit()
+
+  while (grid[currenty][currentx + 1] == "#"):
+    grid[currenty][currentx] = "#"
+    currenty += 1
+    total_moved += 1
+    if (total_moved == input_storage[4] - 1):
+      grid[currenty - 1][currentx] = "M"
+      get_grid()
+      print(currenty + 1, currentx + 1)
+      sys.exit()
+    grid[currenty][currentx] = "M"
 
 
-for Niswanob2 in range(5):
- Niswanob1.append(int(input()))
 
-(fuscat2,bitboostdotcom1)=((Niswanob1[2] + 1),1)
-Chri4=(['#']*    (Niswanob1[0] + 2))
-fuscat1.append(Chri4)
-for fuscat3 in range(Niswanob1[1]):
- bitboostdotcom2=(['']*    (Niswanob1[0] + 2))
- for ordemo1 in range((Niswanob1[0] + 2)):
-  bitboostdotcom2[ordemo1]='.'
-
- fuscat1.append(bitboostdotcom2)
-
-fuscat1.append(Chri4)
-for Niswanob2 in range(1,(Niswanob1[3] + 1)):
- for bitboostdotcom3 in range(1,(Niswanob1[2] + 1)):
-  fuscat1[Niswanob2][bitboostdotcom3]='#'
-  fuscat1[Niswanob2][((len(fuscat1[0]) - bitboostdotcom3) - 1)]='#'
-
-
-for Niswanob2 in range(((len(fuscat1) - Niswanob1[3]) - 1),(len(fuscat1) - 1)):
- for bitboostdotcom3 in range(1,(Niswanob1[2] + 1)):
-  fuscat1[Niswanob2][bitboostdotcom3]='#'
-  fuscat1[Niswanob2][((len(fuscat1[0]) - bitboostdotcom3) - 1)]='#'
-
-
-for Niswanob2 in range(len(fuscat1)):
- (fuscat1[Niswanob2][0],fuscat1[Niswanob2][(len(fuscat1[Niswanob2]) - 1)])=('#','#')
-
-Chri3=0
-fuscat5=0
-fuscat1[bitboostdotcom1][fuscat2]='M'
-for Niswanob2 in range(Niswanob1[4]):
- if (fuscat5==0):
-  if (fuscat1[bitboostdotcom1][(fuscat2 + 1)]!='#'):
-   fuscat1[bitboostdotcom1][fuscat2]='#'
-   fuscat2+=1
-  elif ((fuscat1[bitboostdotcom1][(fuscat2 + 1)]=='#') and (fuscat1[(bitboostdotcom1 + 1)][fuscat2]!='#')):
-   fuscat1[bitboostdotcom1][fuscat2]='#'
-   bitboostdotcom1+=1
-  else:
-   fuscat5=1
-
-
- if (fuscat5==1):
-  if ((fuscat1[bitboostdotcom1][(fuscat2 - 1)]!='#') and (fuscat1[(bitboostdotcom1 + 1)][fuscat2]=='#')):
-   fuscat1[bitboostdotcom1][fuscat2]='#'
-   fuscat2-=1
-  elif ((fuscat1[bitboostdotcom1][(fuscat2 + 1)]=='#') and (fuscat1[(bitboostdotcom1 + 1)][fuscat2]!='#')):
-   fuscat1[bitboostdotcom1][fuscat2]='#'
-   bitboostdotcom1+=1
-  else:
-   fuscat5=2
-
-
- if (fuscat5==2):
-  if ((fuscat1[bitboostdotcom1][(fuscat2 - 1)]=='#') and (fuscat1[(bitboostdotcom1 - 1)][fuscat2]!='#')):
-   fuscat1[bitboostdotcom1][fuscat2]='#'
-   bitboostdotcom1-=1
-  elif ((fuscat1[bitboostdotcom1][(fuscat2 - 1)]!='#') and (fuscat1[(bitboostdotcom1 + 1)][fuscat2]=='#')):
-   fuscat1[bitboostdotcom1][fuscat2]='#'
-   fuscat2-=1
-  else:
-   fuscat5=3
-
-
- if (fuscat5==3):
-  if ((fuscat1[bitboostdotcom1][(fuscat2 + 1)]!='#') and (fuscat1[(bitboostdotcom1 - 1)][fuscat2]=='#')):
-   fuscat1[bitboostdotcom1][fuscat2]='#'
-   fuscat2+=1
-   None
-  elif ((fuscat1[(bitboostdotcom1 - 1)][fuscat2]!='#') and (fuscat1[bitboostdotcom1][(fuscat2 - 1)]=='#')):
-   fuscat1[bitboostdotcom1][fuscat2]='#'
-   bitboostdotcom1-=1
-   None
-  else:
-   fuscat5=0
-   None
-
-
-
-fuscat1[bitboostdotcom1][fuscat2]='M'
-print (fuscat2)
-print (bitboostdotcom1)
-Chri2()
+get_grid()
